74061ce3766f997c86ec5ac570ab9cf213ae7b12
Natalie Ngan <natalie.ngan@fortify.pro>
2017-12-08 08:52:35 -0800

    Added slideshow to home page.

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import backgroundImage from '../assets/nataliealaska-home2.jpg';
+import backgroundImage1 from '../assets/nataliealaska-home1.jpg';
+import backgroundImage2 from '../assets/nataliealaska-home2.jpg';
+import backgroundImage3 from '../assets/nataliealaska-home3.jpg';
+import backgroundImage4 from '../assets/nataliealaska-home4.jpg';
+
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      imageIndex: 0
+    }
+  }
+
+  componentDidMount() {
+      setInterval(() => {
+        let newIndex = (this.state.imageIndex + 1) % 4;
+        console.log("updated index", this.state.imageIndex);
+        this.setState({ imageIndex: newIndex });
+      }, 5000);
+  }
+
   render() {
+    const backgroundImages = [
+      backgroundImage1,
+      backgroundImage2,
+      backgroundImage3,
+      backgroundImage4
+    ]
+
     return (
       <div className="home-bg-wrap">
-        <img className="home-bg" src={ backgroundImage }></img>
+        <img className="home-bg" src={ backgroundImages[this.state.imageIndex] }></img>
       </div>
     );
   }
